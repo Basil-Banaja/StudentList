1e91916ab8e1253e1fdf8767701e477d6e22d012
BasilBanaja <basil.banaja@gmail.com>
2023-12-12 22:47:18 +0300

    First Commit

---
+++ src/studentlist/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentManager manager = new StudentManager();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of students to add:");
+        int numberOfStudents = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.println("Enter name for student " + (i + 1) + ":");
+            String name = scanner.nextLine();
+
+            System.out.println("Enter ID for student " + (i + 1) + ":");
+            String id = scanner.nextLine();
+
+            manager.addStudent(new Student(name, id));
+        }
+
+        System.out.println("\nList of Students:");
+        manager.listStudents();
+
+        scanner.close();
+    }
+}
---
+++ src/studentlist/StudentManager.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author USER
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManager {
+    private List<Student> students;
+
+    public StudentManager() {
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void listStudents() {
+        for (Student student : students) {
+            System.out.println("Student Name: " + student.getName() + ", ID: " + student.getId());
+        }
+    }
+}
